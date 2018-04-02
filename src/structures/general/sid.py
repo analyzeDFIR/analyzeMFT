86432dc56c7a6c6e00430c8f2f339a6b99bdb5d7
noahrubin <noahski@benrubin.net>
2018-04-02 00:18:45 -0500

    added function to convert SID identifier string to Container

--- src/structures/general/sid.py
+++ src/structures/general/sid.py
@@ -50,11 +50,31 @@ def sid_to_string(sid):
     Args:
         sid: NTFSSID    => sid struct to convert to string
     Returns:
-        String containing SID string in the form shown above
+        String
+        String representation of SID string in the form shown above
     Preconditions:
         sid is instance of NTFSSID
     '''
-    return '-'.join(
+    return '-'.join(\
         ['S', str(sid.Revision), str(sid.Authority)] + \
-        [str(sub_authority) for sub_authority in sid.SubAuthorities]
+        [str(sub_authority) for sub_authority in sid.SubAuthorities]\
     ) 
+
+def string_to_sid(identifier):
+    '''
+    Args:
+        identifier: String  => sid string to convert to NTFSSID
+    Returns:
+        Container<String, Any>
+        Struct representation of String SID identifier
+    Preconditions:
+        identifier is of the format specified above (assumed True)
+    '''
+    split_identifier = identifier.strip().split('-')
+    sub_authorities = [int(subauth) for subauth in split_identifier[3:]]
+    return Container(\
+        Revision=int(split_identifier[1]),
+        SubAuthoritiesCount=len(sub_authorities),
+        Authority=int(split_identifier[2]),
+        SubAuthorities=sub_authorities\
+    )
